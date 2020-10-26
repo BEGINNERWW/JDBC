50d975673f330b3bae446a932687076827e61bb0
BEGINNERWW <ivedot@naver.com>
2020-10-26 18:56:45 +0900

    Eclipse_Oracle connect (SQL_DML SELECT)

---
+++ SelectExam.java
@@ -0,0 +1,50 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SelectExam {
+
+	public static void main(String[] args) {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+		Connection con =null;
+		Statement stmt = null;
+		//PreparedStatement stmt =null;
+		ResultSet rs = null;
+		
+		String sql = "Select * From member order by hakbun";
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url,"scott","123456");
+			
+			stmt = con.createStatement();
+			//stmt = con.prepareStatement(sql);
+			rs = stmt.executeQuery(sql); //ResultSet 객체 반환
+			//rs = stmt.executeQuery();
+			
+			System.out.println("hakbun\tname\taddr\tphone");
+			System.out.println("-----------------------------------------------");
+			while(rs.next()) {
+				System.out.print(rs.getNString("hakbun")+"\t");
+				System.out.print(rs.getNString("name")+"\t");
+				System.out.print(rs.getNString("addr")+"\t");
+				System.out.print(rs.getNString("phone")+"\n");
+			}
+		}
+		catch(Exception e) {
+			System.out.println("데이터베이스 연결 실패!");
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(rs!=null) rs.close();
+				if(stmt !=null) stmt.close();
+				if(con != null) con.close();
+			}
+			catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
