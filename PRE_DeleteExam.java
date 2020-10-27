a4f7f66a537f1c8ad025e9ff6de8aea34a9b5cf6
BEGINNERWW <73094693+BEGINNERWW@users.noreply.github.com>
2020-10-27 19:38:05 +0900

    JDBC_PreparedStatement

---
+++ PRE_DeleteExam.java
@@ -0,0 +1,50 @@
+package day1027;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class DeleteExam {
+
+	public static void main(String[] args) {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+		Connection con =null;
+		PreparedStatement stmt = null;
+		
+		String hakbun = null;
+		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Member 테이블 값 삭제하기.....");
+			System.out.print("삭제할 학번 입력 : ");
+			hakbun = br.readLine();
+			
+			String sql = "Delete From member Where hakbun = ?";
+			System.out.println(sql);
+			
+			Class.forName(driver);
+			con = DriverManager.getConnection(url,"scott","123456");
+			//con.setAutoCommit(false);
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, hakbun);
+			stmt.executeUpdate();
+			
+			System.out.println("데이터베이스 내용 삭제 완료!");
+			//con.commit();
+		}
+		catch(Exception e) {
+			System.out.println("데이터베이스 연결 실패 !"+ e.getMessage());
+		}
+		finally {
+			try {
+				if(stmt !=null) stmt.close();
+				if(con != null) con.close();
+			}
+			catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+}
---
+++ PRE_UpdateExam.java
@@ -0,0 +1,57 @@
+package day1027;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class UpdateExam {
+
+	public static void main(String[] args) {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+		Connection con =null;
+		PreparedStatement stmt = null;
+		
+		String hakbun, addr, phone;
+		
+		try {
+			BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+			System.out.println("Member 테이블에 값 수정하기.....");
+			System.out.print("수정할 학번 입력 : ");
+			hakbun = br.readLine();
+			System.out.print("새 주소 입력 : ");
+			addr = br.readLine();
+			System.out.print("새 핸드폰번호 입력 : ");
+			phone = br.readLine();
+			
+			//String sql = "Update member Set addr = '"+ addr;
+			//sql += "', phone = '" + phone + "' Where hakbun ='";
+			//sql += hakbun +"'";
+			String sql = "Update member Set addr = ?, phone= ? Where trim(hakbun) = ?";
+			System.out.println(sql);
+			Class.forName(driver);
+			con = DriverManager.getConnection(url,"scott","123456");
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, addr);
+			stmt.setString(2, phone);
+			stmt.setString(3, hakbun);
+			stmt.executeUpdate();
+			System.out.println("데이터베이스 내용 수정 완료!");
+		}
+		catch(Exception e) {
+			System.out.println("데이터베이스 연결 실패 !"+ e.getMessage());
+		}
+		finally {
+			try {
+				if(stmt !=null) stmt.close();
+				if(con != null) con.close();
+			}
+			catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+	}
+
+}
